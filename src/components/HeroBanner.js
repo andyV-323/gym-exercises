bb5c498469f1475b12f22c90468d768bfdaea262
andyV-323 <40248914+andyV-323@users.noreply.github.com>
2024-01-04 16:20:58 -0800

    styles and navigate

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,79 +2,90 @@ import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import HeroBannerImage from '../assets/images/banner.png';
+import { useNavigate } from 'react-router-dom';
 
-const HeroBanner = () => (
-  <Box
-    sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }}
-    position="relative"
-    p="20px"
-  >
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-      Fitness Club
-    </Typography>
-    <Typography
-      fontWeight={700}
-      sx={{ fontSize: { lg: '44px', xs: '40px' } }}
-      mb="23px"
-      mt="30px"
+const HeroBanner = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }}
+      position="relative"
+      p="20px"
     >
-      Sweat, Smile <br />
-      And Repeat
-    </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
-    </Typography>
-    <Stack>
-      <a
-        href="#exercises"
-        style={{
-          marginTop: '45px',
-          textDecoration: 'none',
-          width: '200px',
-          textAlign: 'center',
-          background: '#FF2625',
-          padding: '14px',
-          fontSize: '22px',
-          textTransform: 'none',
-          color: 'white',
-          borderRadius: '4px',
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '40px' } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, Smile <br />
+        And Repeat
+      </Typography>
+      <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+        Check out the most effective exercises personalized to you
+      </Typography>
+      <Stack>
+        <a
+          href="#exercises"
+          style={{
+            marginTop: '45px',
+            textDecoration: 'none',
+            width: '200px',
+            textAlign: 'center',
+            background: '#FF2625',
+            padding: '13px',
+            fontSize: '22px',
+            textTransform: 'none',
+            color: 'white',
+            borderRadius: '4px',
+            borderStyle: 'none',
+          }}
+        >
+          Explore Exercises
+        </a>
+      </Stack>
+      <Stack>
+        <button
+          onClick={() => navigate('/calculators')}
+          style={{
+            marginTop: '45px',
+            textDecoration: 'none',
+            width: '200px',
+            textAlign: 'center',
+            background: '#FF2625',
+            padding: '14px',
+            fontSize: '22px',
+            textTransform: 'none',
+            color: 'white',
+            borderRadius: '4px',
+            fontFamily: 'Josefin Sans',
+          }}
+        >
+          Fitness Calculators
+        </button>
+      </Stack>
+      <Typography
+        fontWeight={600}
+        color="#FF2625"
+        sx={{
+          opacity: '0.1',
+          display: { lg: 'block', xs: 'none' },
+          fontSize: '200px',
         }}
       >
-        Explore Exercises
-      </a>
-    </Stack>
-    <Stack>
-      <a
-        href="/calculators"
-        style={{
-          marginTop: '45px',
-          textDecoration: 'none',
-          width: '200px',
-          textAlign: 'center',
-          background: '#FF2625',
-          padding: '14px',
-          fontSize: '22px',
-          textTransform: 'none',
-          color: 'white',
-          borderRadius: '4px',
-        }}
-      >
-        Fitness Calculators
-      </a>
-    </Stack>
-    <Typography
-      fontWeight={600}
-      color="#FF2625"
-      sx={{
-        opacity: '0.1',
-        display: { lg: 'block', xs: 'none' },
-        fontSize: '200px',
-      }}
-    >
-      Exercise
-    </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
-  </Box>
-);
+        Exercise
+      </Typography>
+      <img
+        src={HeroBannerImage}
+        alt="hero-banner"
+        className="hero-banner-img"
+      />
+    </Box>
+  );
+};
 
 export default HeroBanner;
